7b2fb7f1b1147148e1c864ecd9f9ecacfc1418fb
jesmehta <jesmehta@gmail.com>
2020-02-04 18:35:59 +0530

    Random Walk added
    Basic random walk, Random walk that wraps around canvas with line trail

---
+++ 07_Perlin Noise/Random walk basic/sketch.js
@@ -0,0 +1,24 @@
+let x0, y0;
+let step;
+
+  function setup() {
+    createCanvas(400, 400);
+    x0 = width / 2;
+    y0 = height / 2;
+    step = 50;
+    fill(0);
+  }
+
+function draw() {
+  background(255,10);
+  let x = random(-step, +step);
+  let y = random(-step, +step);
+
+  x0 += x;
+  y0 += y;
+  ellipse(x0, y0, 5, 5);
+
+  x0 = abs(x0%width);
+  y0 = abs(y0%height);
+
+}
